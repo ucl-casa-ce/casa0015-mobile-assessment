1ed0361ea912db91f490a3d7aca835bbba1cbced
mfoster0 <mark.foster@live.co.uk>
2024-02-20 18:50:33 +0000

    AppNavigation Test

---
+++ calmer_beats003/android/app/src/main/kotlin/mfoster/example/calmer_beats003/MainActivity.kt
@@ -0,0 +1,5 @@
+package mfoster.example.calmer_beats003
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ test_code/android/app/src/main/kotlin/mfoster/example/test_code/MainActivity.kt
@@ -0,0 +1,5 @@
+package mfoster.example.test_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
