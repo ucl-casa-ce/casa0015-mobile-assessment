eae43513d0f56bc244b59cea425526b1f90113a4
mfoster0 <mark.foster@live.co.uk>
2024-02-26 16:47:07 +0000

    bluetooth test

---
+++ bluetooth_example/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:bluetooth_example/ble_controller.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BLE SCANNER"),
+        centerTitle: true,
+      ),
+      body: GetBuilder<BleController>(
+        init: BleController(),
+        builder: (controller) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                StreamBuilder<List<ScanResult>>(
+                    stream: controller.scanResults,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              final data = snapshot.data![index];
+                              return Card(
+                                elevation: 2,
+                                child: ListTile(
+                                  title: Text(data.device.name),
+                                  subtitle: Text(data.device.id.id),
+                                  trailing: Text(data.rssi.toString()),
+                                ),
+                              );
+                            });
+                      } else {
+                        return Center(
+                          child: Text("No Device Found"),
+                        );
+                      }
+                    }),
+                ElevatedButton(
+                    onPressed: () => controller.scanDevices(),
+                    child: Text("Scan")),
+                SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
